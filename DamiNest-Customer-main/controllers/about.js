9b906212d9a3e9ae81085ebc1caa8f837d606e9d
dung.nq2 <dungnguyen310701@gmail.com>
2024-02-22 19:13:13 +0700

    delete useless code

--- DamiNest-Customer-main/controllers/about.js
+++
@@ -1,7 +0,0 @@
-const index = (req, res) => {
-  res.render('about/index')
-}
-
-module.exports = {
-  index
-}
--- DamiNest-Customer-main/utils/currency.js
+++
@@ -1,8 +0,0 @@
-const currencyFormatter = new Intl.NumberFormat('vi-VN', {
-  style: 'currency',
-  currency: 'VND'
-})
-
-module.exports = {
-  format: currencyFormatter.format
-}
